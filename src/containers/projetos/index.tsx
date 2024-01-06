cb6d2164ec016ec7e3c6e2b0790a1bf7678979fd
João Victor Ragazzi <jvictorragazzi@gmail.com>
2024-01-05 21:58:48 -0300

    estilizando a sidebar

--- src/containers/projetos/index.tsx
+++ src/containers/projetos/index.tsx
@@ -26,6 +26,18 @@ const Projetos = () => (
         <li>
           <Projeto />
         </li>
+        <li>
+          <Projeto />
+        </li>
+        <li>
+          <Projeto />
+        </li>
+        <li>
+          <Projeto />
+        </li>
+        <li>
+          <Projeto />
+        </li>
       </Lista>
     </section>
   </div>
--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/jvragazzi.png" />
-    <Titulo fontSize={20}>João Victor Ragazzi</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>João Victor Ragazzi</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        @jvragazzi
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Frontend
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
