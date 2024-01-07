8f12adcddbca3abd6f9e615003a69113c58f376d
João Victor Ragazzi <jvictorragazzi@gmail.com>
2024-01-07 07:46:31 -0300

    finalizando

---
+++ src/themes/dark.ts
@@ -0,0 +1,17 @@
+const temaDark = {
+  corPrincipal: '#fff',
+  corSecundaria: '#949494',
+  corFundo: '#282a35',
+  corFundoBotao: '#4476bf',
+  corBorda: '#c1c1c1'
+}
+
+export default temaDark
+
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+  corFundo: string
+  corFundoBotao: string
+  corBorda: string
+}
